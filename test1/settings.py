05aab6b096888dee30945c53b8dd9c118ac8e2a1
Abishek Madhav <143243139+Abishek-Madhav@users.noreply.github.com>
2025-01-09 08:50:19 +0530

    Update settings.py

--- test1/settings.py
+++ test1/settings.py
@@ -14,7 +14,7 @@ if not SECRET_KEY:
 DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
 
 # Allowed Hosts
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1,musi1.onrender.com").split(",")
 
 # Application definition
 INSTALLED_APPS = [
