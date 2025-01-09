4f7fccc272759a002deff5f355c3a47ba0f58e40
Abishek Madhav <143243139+Abishek-Madhav@users.noreply.github.com>
2025-01-09 11:56:23 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -3,9 +3,10 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
-# Specify the relative path to the service_account.json file
-SERVICE_ACCOUNT_PATH = os.path.join(os.path.dirname(__file__), "../service_account.json")
+# Specify the absolute path to the service_account.json file
+SERVICE_ACCOUNT_PATH = "/etc/secrets/service_account"
 
+# Check if the service account file exists
 if not os.path.exists(SERVICE_ACCOUNT_PATH):
     raise FileNotFoundError(f"Service account file not found at: {SERVICE_ACCOUNT_PATH}")
 
